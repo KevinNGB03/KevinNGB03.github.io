f77da79d78da7507b9b9e82bb376fadfa712b8cd
KevinNGB03 <satinoke@gmail.com>
2023-10-01 11:59:58 -0500

    v1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+let hideText_btn = document.getElementById('hideText_btn');
+
+let hideText = document.getElementById('hideText');
+
+
+hideText_btn.addEventListener('click', ToggleText);
+
+function ToggleText() {
+    hideText.classList.toggle('show');
+
+
+if(hideText.classList.contains('show')) {
+    hideText_btn.innerHTML = 'Leer menos';
+}
+else {
+    hideText_btn.innerHTML = 'Leer mas';
+}
+}
